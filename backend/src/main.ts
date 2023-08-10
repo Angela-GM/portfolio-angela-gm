037e47ef96fb00a0e1c60471a61e8bf0ca99c441
Angela-GM <angelagm0112@gmail.com>
2023-08-10 17:19:45 +0200

    configure swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('Portfolio Angela API')
+    .setDescription('Documentation of Portfolio Angela API')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // Configuración del ENDPOINT de la documentación de la API en /docs
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
