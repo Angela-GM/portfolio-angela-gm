9d666a84fc80a25251aa7cd8c781f9337b9eb258
Angela-GM <angelagm0112@gmail.com>
2023-08-20 20:15:08 +0200

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigModule } from '@nestjs/config';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
