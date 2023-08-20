1feb6dff4c63cc5c8b2cf3a31ff862d6df1f7cdb
Angela-GM <angelagm0112@gmail.com>
2023-08-20 18:23:35 +0200

    terminar estructura y estilos home, falta animaciones

--- frontend/src/components/routes/Home.tsx
+++ frontend/src/components/routes/Home.tsx
@@ -1,22 +1,43 @@
-import { TfiGithub, TfiLinkedin} from 'react-icons/tfi'
+import { TfiGithub, TfiLinkedin } from "react-icons/tfi";
+import { FaFilePdf } from 'react-icons/fa'
 function Home() {
   return (
     <div className="container min-h-screen2 mx-auto max-w-screen-xl dark:bg-bg-primary dark:text-text-primary overflow-y-hidden">
       <div className="flex flex-row w-full justify-between items-center my-6">
         <div className="w-1/2">
-          <h1 className="font-bold text-5xl">Hola, soy Angela GM;</h1>
-          <h2 className="text-teal-500 font-semibold text-2xl">FullStack Developer;</h2>
-          <p>
-            Tras descubrir mi pasión en la programación, busco incansablemente crear aplicaciones que fusionen la creatividad y la tecnología para resolver desafíos significativos. Mi objetivo es contribuir al mundo con soluciones innovadoras y impactantes.
+          <h1 className="font-bold text-5xl my-4">Hola, soy Angela GM<span className="text-teal-500">;</span></h1>
+          <h2 className="text-teal-500 font-semibold text-2xl my-4">
+            FullStack Developer;
+          </h2>
+          <p className="my-4">
+            Tras descubrir mi pasión en la programación, busco incansablemente
+            crear aplicaciones que fusionen la creatividad y la tecnología para
+            resolver desafíos significativos. Mi objetivo es contribuir al mundo
+            con soluciones innovadoras y impactantes.
           </p>
-          <a href='https://github.com/Angela-GM' target='_blank'><TfiGithub /></a>
-          <a href='https://www.linkedin.com/in/angela-garcia-mu/' target='_blank'><TfiLinkedin /></a>
-          <a href="../../../public/cv-angela-garcia-fullstack.pdf" download="cv-angela-gm-dev-fullstack.pdf">
-  <button>CV</button>
-</a>
+          <div className="flex gap-3 text-teal-500 items-center text-2xl">
+            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300" href="https://github.com/Angela-GM" target="_blank">
+              <TfiGithub />
+            </a>
+            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
+              href="https://www.linkedin.com/in/angela-garcia-mu/"
+              target="_blank"
+            >
+              <TfiLinkedin />
+            </a>
+            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
+              href="../../../public/cv-angela-garcia-fullstack.pdf"
+              download="cv-angela-gm-dev-fullstack.pdf"
+            >
+              <FaFilePdf />
+            </a>
+          </div>
         </div>
         <div>
-          <img src="./../../../public/image-dev.svg" alt="image computer programming" />
+          <img
+            src="./../../../public/image-dev.svg"
+            alt="image computer programming"
+          />
         </div>
       </div>
     </div>
