f91a4a04232c4f7bb416caec665d2d4f2c026228
Angela-GM <angelagm0112@gmail.com>
2023-08-20 20:17:58 +0200

    arreglar cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin:
-      process.env.FRONTEND_URL ||
-      'http://localhost:3000' ||
+    origin: [
+      process.env.FRONTEND_URL,
+      'http://localhost:3000',
       'http://localhost:5173',
+    ],
     credentials: true,
   });
 
