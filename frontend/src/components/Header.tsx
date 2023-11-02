ac583b486506537007e9b9655f2e391d0c9ee1fc
Angela-GM <angelagm0112@gmail.com>
2023-11-02 15:58:30 +0100

    update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,6 +13,54 @@ function Header({ toggleTheme, theme }: HeaderProps) {
         className="container py-5 mx-auto"
         style={{ opacity: 1, transform: "none" }}
       >
+        {/* Inicio menu hamburguesa */}
+        <nav className="md:hidden">
+          {/* Boton claro/oscuro */}
+        <button
+              className="ml-1 font-bold py-2 px-3 rounded dark:text-white text-2xl transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500 hover:-translate-y-2 "
+              onClick={toggleTheme}
+            >
+              {theme === "dark" ? <MdLightMode /> : <MdDarkMode />}
+            </button>
+            {/* Fin boton claro/oscuro */}
+        <Link to="/">
+                <div className="transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500  hover:-translate-y-2">
+                  Home
+                </div>
+              </Link>
+              <Link               
+                to="/projects"
+              >
+                <div className="transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500  hover:-translate-y-2">Proyectos</div>
+                
+              </Link>
+
+              <Link
+                
+                to="/about"
+              >
+                <div className="transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500  hover:-translate-y-2">About</div>
+                
+              </Link>
+              <Link
+                
+                to="/contact"
+              ><div className="transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500  hover:-translate-y-2">Contacto</div>
+                
+              </Link>
+              <a
+                
+                href="https://medium.com/@angelagmGM"
+                target="_blank"
+              >
+                <div className="transition ease-in-out delay-120 duration-300 dark:hover:text-teal-500 hover:text-teal-500  hover:-translate-y-2" >Blog</div>
+                
+              </a>
+          
+        </nav>
+
+        {/* Fin menu hamburguesa */}
+        {/* Inicio menu */}
         <div className="container mx-auto max-w-screen-xl hidden h-20 items-center md:flex justify-between">
           <Link to={"/"} className="hidden text-2xl font-bold md:inline-block">
             Angela GM<span className="text-teal-500"> ;</span>
@@ -61,6 +109,9 @@ function Header({ toggleTheme, theme }: HeaderProps) {
             </button>
           </div>
         </div>
+        {/* fin menu */}
+
+
       </div>
     </header>
   );
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -4,7 +4,7 @@ import imgdev  from '../assets/image-dev.svg'
 import cvPdf from '../assets/cv-angela-garcia-fullstack.pdf'
 function Home() {
   return (
-    <div className="container min-h-screen2 mx-auto max-w-screen-xl dark:bg-bg-primary dark:text-text-primary ">
+    <div className="container min-h-screen2 max-w-screen-xl mx-7 dark:bg-bg-primary dark:text-text-primary flex">
       <div className="flex flex-row w-full justify-between items-center my-6">
         <div className="w-1/2">
           <h1 className="font-bold text-5xl my-4">Hola, soy Angela GM<span className="text-teal-500"> ;</span></h1>
@@ -18,16 +18,16 @@ function Home() {
             con soluciones innovadoras y impactantes.
           </p>
           <div className="flex gap-3 text-teal-500 items-center text-2xl">
-            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300" href="https://github.com/Angela-GM" target="_blank">
+            <a className="rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300" href="https://github.com/Angela-GM" target="_blank">
               <TfiGithub />
             </a>
-            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
+            <a className="rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
               href="https://www.linkedin.com/in/angela-garcia-mu/"
               target="_blank"
             >
               <TfiLinkedin />
             </a>
-            <a className=" rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
+            <a className="rounded text-teal-500 p-2 hover:bg-teal-500 hover:text-white transition duration-300"
               href={cvPdf}
               download="cv-angela-gm-dev-fullstack.pdf"
             >
